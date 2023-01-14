95f9b8f10cb62aa72292d98654a8554515af44d9
ssahil369 <114914318+ssahil369@users.noreply.github.com>
2023-01-15 00:52:12 +0530

    Added some comments
    Comments  were added to give the User a bit more knowledge about the code

--- scripting.py
+++ scripting.py
@@ -3,10 +3,14 @@ import pyautogui as pi
 # pi.moveTo(100,100,duration = 1)  //It is used to move the mouse in the given location
 # pi.moveRel(100,200,duration = 2) //It is  move the cursor to its relative position
 # print(pi.position()) //It gives the position of the cursor
-pi.click(1241,1056,duration=0.1)
+pi.click(1241,1056,duration=0.1) #It will click at that location which is given
 
 b=1
 while(b!=51):
     pi.typewrite("Thanks For Checking Out My code")
     pi.typewrite(["Enter"])
     b=b+1
+   
+#For the co-ordinates, i want you to use your brain and think how can we find the co-ordinates.
+#ThankYou 
+# P.S ALL THE BEST
