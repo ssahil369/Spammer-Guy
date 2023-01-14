3c5ed9a0b253616a4e9589ba9e5bdf7264a3b55c
ssahil369 <114914318+ssahil369@users.noreply.github.com>
2023-01-15 00:48:10 +0530

    scripting.py contains the code of the irritation.

---
+++ scripting.py
@@ -0,0 +1,12 @@
+import pyautogui as pi
+# print(pi.size())  //This function is used to get the screen resolution
+# pi.moveTo(100,100,duration = 1)  //It is used to move the mouse in the given location
+# pi.moveRel(100,200,duration = 2) //It is  move the cursor to its relative position
+# print(pi.position()) //It gives the position of the cursor
+pi.click(1241,1056,duration=0.1)
+
+b=1
+while(b!=51):
+    pi.typewrite("Thanks For Checking Out My code")
+    pi.typewrite(["Enter"])
+    b=b+1
